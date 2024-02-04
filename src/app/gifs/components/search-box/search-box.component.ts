82342acf37a42408332b118465e75708b3ff372e
Pedro <pedrohm92@gmail.com>
2024-02-04 17:17:22 +0100

    Creación de servicio para almacenar strings buscados

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
     <input type="text"
       class="form-control"
       placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
     />
   `
 })
 
 export class SearchBoxComponent {
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
+
+  @ViewChild("txtTagInput")
+  tagInput!: ElementRef<HTMLInputElement>
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value
+    this.gifsService.searchTag(newTag)
+    this.tagInput.nativeElement.value = ""
+  }
 }
