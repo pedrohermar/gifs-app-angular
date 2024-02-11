496a049be11221cec96770d373defa458a0447dd
Pedro <pedrohm92@gmail.com>
2024-02-11 19:10:48 +0100

    historial de búsquedas guardado en localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,42 +11,56 @@ export class GifsService {
   private apiKey: string = "DDheyBPzVmUDpvN70Zt6iiFUIv1NIUlV"
   private serviceUrl: string = "https://api.giphy.com/v1/gifs"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  };
 
   get tagsHistory(): string[] {
-    return [...this._tagsHistory]
-  }
+    return [...this._tagsHistory];
+  };
 
   private organizeHistory(tag: string) {
-    tag = tag.toLowerCase()
+    tag = tag.toLowerCase();
 
     if (this._tagsHistory.includes(tag)) {
-      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag)
-    }
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    };
 
-    this._tagsHistory.unshift(tag)
-
-    this._tagsHistory = this._tagsHistory.splice(0, 10)
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  };
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0])
+  };
+
   searchTag(tag: string): void {
-    if (tag.length === 0) return
-    this.organizeHistory(tag)
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
 
     const params = new HttpParams()
       .set('api_key', this.apiKey)
       .set('q', tag)
-      .set('limit', '10')
+      .set('limit', '10');
 
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
       .subscribe(resp => {
 
         this.gifList = resp.data
 
-      })
+      });
 
     // fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`)
     //   .then(resp => resp.json())
     //   .then(data => console.log(data))
-  }
-}
+  };
+};
