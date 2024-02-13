8fb6121f50c4c7c96c1a98b440c2cb3a11c8346a
Pedro <pedrohm92@gmail.com>
2024-02-13 23:40:50 +0100

    Agregado loader para la carga de gifs

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  url!: string;
+
+  @Input()
+  alt: string = "";
+
+  hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property is required');
+  }
+
+  onLoad() {
+    console.log('Image Loaded')
+    this.hasLoaded = true;
+  }
+
+}
